68102cf2f2bf8717be1e3371a70ab7884f1a2c0a
raghavendra-musubi <raghavendra@techis.io>
2020-11-22 00:58:48 +0530

    unit test setup begins

---
+++ unittests.py
@@ -0,0 +1,24 @@
+from matrix import Matrix
+import unittest
+
+# define a vector list
+list_a = [1,2,3,4]
+
+# define a nested list for matrix 
+list_b = [[1],[2],[3],[4]]
+
+# define a nested list for matrix with 
+list_c = [[1],[2],[],[4]]
+
+# define a non-numerical fully list
+list_d = [2,3,[],4]
+
+# a = Matrix([2,3,1,4])
+# print(a)
+# print(type(a))
+# print(Matrix.is_numeric_list([2,3,[],4]))
+
+# b = Matrix(list_b)
+# print(b)
+# print(type(b))
+# print(Matrix.is_nested_list(list_a))
